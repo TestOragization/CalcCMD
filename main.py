249a7b8f4538e7dd9293f41a3febd2ea5cc3c275
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-03-05 13:06:18 +0200

    Created functional for cmd

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello")
+from Calc import Calculator
+
+calc = Calculator()
+
+while True:
+    print("\n---Калькулятор (CMD)---")
+    print("1. Математ. выражение")
+    print("2. Показать историю")
+    print("3. Показать историю логов пользователя")
+    print("4. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        expression = input("Enter expression: ")
+        result = calc.calculate(expression)
+        print(f"Result: {result}")
+    elif choice == "2":
+        calc.show_history()
+    elif choice == "3":
+        calc.show_user_logs()
+    elif choice == "4":
+        calc.close()
+        break
+    else:
+        print("Error")
